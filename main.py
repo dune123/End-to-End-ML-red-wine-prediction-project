31677ec4d0d6d5f7b6df0a08e5123d1e9dcfd830
sraj39273@gmail.com <sraj39273@gmail.com>
2023-10-21 21:39:51 +0530

    data ingestion

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.mlproject.logging import logger
+from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME='Data Ingestion Stage'
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
